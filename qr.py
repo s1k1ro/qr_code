e10fa5436d0cfaaea3ae6de14c2743fc1983dd10
s1k1ro <sikiro111111@gmail.com>
2026-06-19 23:08:13 +1000

    v1: encode data from command line

--- qr.py
+++ qr.py
@@ -1,3 +1,19 @@
 import qrcode
-img = qrcode.make("hello world")
-img.save("qr.png")
+import argparse
+
+def main():
+    #create the parser object
+    parser = argparse.ArgumentParser(description="Generate QR Code base on CLI input")
+
+    #first postiional argument
+    parser.add_argument("url", help="The URL to encode")
+    args = parser.parse_args()
+
+    img = qrcode.make(args.url)
+    img.save("qr.png")
+
+if __name__ == "__main__":
+    main()
+
+#img = qrcode.make("hello world")
+#oimg.save("qr.png")
