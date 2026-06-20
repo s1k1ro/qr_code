516c0d519dfd813cf56ef44779b0ef36d502da86
s1k1ro <sikiro111111@gmail.com>
2026-06-20 13:13:55 +1000

    v2: output path option, input validation and save error handling

--- qr.py
+++ qr.py
@@ -7,10 +7,20 @@ def main():
 
     #first postiional argument
     parser.add_argument("url", help="The URL to encode")
+    #optional arguments
+    parser.add_argument("-o", "--output", help="output file name and type e.g myqr.png", default="qr.png")
     args = parser.parse_args()
 
+    if not args.url.strip (): #.strip()removes spaces so "  " also becomes false
+        print("You are missing the url")
+        return
+    
     img = qrcode.make(args.url)
-    img.save("qr.png")
+    try:
+        img.save(args.output)
+        print(f"Successfully saved {args.output}")
+    except Exception as e:
+        print(f"Error: Couldnt save file. {e}")
 
 if __name__ == "__main__":
     main()
