cdf496e8dc7195fa24754889a108a0d4bc9871a4
s1k1ro <sikiro111111@gmail.com>
2026-06-25 19:43:07 +1000

    v3.1, update default back and fill colour and removed scratch file from commit

--- qr.py
+++ qr.py
@@ -16,8 +16,8 @@ def main():
     parser.add_argument("-v", "--version", type=int,  help="The version parameter is an int from 1 to 40 that cotnrols the size of the QR Code (smallest is 1)", default=None)
     parser.add_argument("-s", "--size", type=int, help="the size parameter controls how many pixels each 'box' of the QR code is", default = 10)
     parser.add_argument("-b","--border", type=int,  help="the border paramater contols how many boxes thick the border should be the (default is 4 minimum from specs)", default=4)
-    parser.add_argument("-bc","--back-colour", help="control background colour, accepts colour name in string format", default="black")
-    parser.add_argument("-fc","--fill-colour", help="control fill colour, accepts colour name in string format", default="white")
+    parser.add_argument("-bc","--back-colour", help="control background colour, accepts colour name in string format", default="white")
+    parser.add_argument("-fc","--fill-colour", help="control fill colour, accepts colour name in string format", default="black")
     parser.add_argument("-l", "--level", choices=["L","M","Q","H"], default = "L", help="Error correction level (L, M, Q, H)"  )
     
     args = parser.parse_args()
